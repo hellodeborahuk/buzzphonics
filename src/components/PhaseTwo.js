74c2deb927fc842aa5d647c38586e1222f1b3780
Debbie Dann <29425781+hellodeborahuk@users.noreply.github.com>
2022-02-03 10:05:07 +0000

    Added styling to sounds

--- src/components/PhaseTwo.js
+++ src/components/PhaseTwo.js
@@ -3,20 +3,42 @@ const PhaseTwo = () => {
     const sounds = [
         {
             letter: "s",
-            sound: "/"
+            sound: "/", 
+            index: 1
         },
         {
             letter: "a",
-            sound: "/"
+            sound: "/", 
+            index: 2
         },
         {
             letter: "t",
-            sound: "/"
+            sound: "/", 
+            index: 3
+        },
+        {
+            letter: "p",
+            sound: "/", 
+            index: 3
+        },
+        {
+            letter: "i",
+            sound: "/", 
+            index: 3
+        },
+        {
+            letter: "n",
+            sound: "/", 
+            index: 3
         },
     ]
     return ( 
         <section>
             <h1>Phase Two</h1>
+            <div className="sound-container">
+            {sounds.map((sound, index) =>
+            <div className="sound"><p>{sound.letter}</p></div>)}
+            </div>
         </section>
      );
 }
