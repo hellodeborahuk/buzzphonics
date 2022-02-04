f40c92226563bbc6f1bda148eaf48fb1ebc2e612
Debbie Dann <29425781+hellodeborahuk@users.noreply.github.com>
2022-02-04 13:33:47 +0000

    created handle answer function

--- src/components/PhaseTwoQuiz.js
+++ src/components/PhaseTwoQuiz.js
@@ -1,9 +1,8 @@
 import { MdArrowBackIosNew } from "react-icons/md"
 import { Link } from "react-router-dom"
+import { useState } from "react"
 
 const PhaseTwoQuiz= () => {
-
-
     const quizQuestions = [
         {
             id: 0,
@@ -14,46 +13,78 @@ const PhaseTwoQuiz= () => {
                 {answerText: "f", isCorrect: false},
                 {answerText: "ck", isCorrect: false},
             ]
-        }
+        },
+        {
+            id: 1,
+            image: "king",
+            alt: "crown",
+            answerOptions: [
+                {answerText: "c", isCorrect: true},
+                {answerText: "h", isCorrect: false},
+                {answerText: "ck", isCorrect: false},
+            ]
+        },
+        {
+            id: 2,
+            image: "frog",
+            alt: "frog",
+            answerOptions: [
+                {answerText: "a", isCorrect: false},
+                {answerText: "f", isCorrect: true},
+                {answerText: "p", isCorrect: false},
+            ]
+        },
     ]
+    const [currentQuestion, setCurrentQuestion] = useState(0)
+    const [displayEndPage, setdisplayEndPage] = useState(false)
     const alternatingColor = ["var(--pink)", "var(--green)", "var(--yellow)", "var(--blue)", "var(--light-blue)", "var(--purple)"]
+    const randomElement = () => {
+        return {backgroundColor: alternatingColor[Math.floor(Math.random() * alternatingColor.length)]}
+    }
 
+    const handleNextBtnClick = () => {
+        const nextQuestion = currentQuestion + 1
+        if (nextQuestion < quizQuestions.length) {
+            setCurrentQuestion(nextQuestion)
+        }
+        else {
+            setdisplayEndPage(true)
+        }
+    }
+
+    const handleAnswer = (isCorrect) => {
+       if (isCorrect === true) {
+        console.log("true");
+       } else {
+           console.log("flase")
+       }
+    }
     return ( 
         <section>
                 <header>    
                 <Link to="/"><MdArrowBackIosNew className="back-arrow" /></Link>
                 <h1>Phase Two Quiz</h1>
             </header>
-            <div className="quiz-container">
-                {
-                    quizQuestions.map((quizQuestion, index) => {
-                        const randomElement = () => {
-                            return {backgroundColor: alternatingColor[Math.floor(Math.random() * alternatingColor.length)]}
+            {displayEndPage ? (
+                <div>Well done!</div>
+            ) : (
+                <>
+                <div className="quiz-container">
+                    <div className="quiz-image-container" style={randomElement()}>
+                        <img className="quiz-image" src={`../images/${quizQuestions[currentQuestion].image}.png`} alt={quizQuestions[currentQuestion].alt} />
+                    </div>
+                    <div className="quiz-options">
+                        { quizQuestions[currentQuestion].answerOptions.map((answer, index) => {                                            return (
+                            <div className="sound" style={randomElement()} onClick={() => handleAnswer(answer.isCorrect)}>
+                                <p>{answer.answerText}</p>
+                            </div>
+                            )})
                         }
-                        return (
-                            <>
-                                <div className="quiz-image-container" style={randomElement()}>
-                                    <img className="quiz-image" src={`../images/${quizQuestion.image}.png`} alt={quizQuestion.alt} />
-                                    </div>
-                                    <div className="quiz-options">
-                                        { quizQuestions[0].answerOptions.map((answer, index) => {
-                                            console.log(answer.answerText)
-                                            return (
-                                                <div className="sound" style={randomElement()}>
-                                                <p>{answer.answerText}</p>
-                                            </div>
-                                            )
-                                        })
-                                    }
-                                    </div>
-                                <button className="next-btn">Next</button>
-                             </>
-                       )
-                    })
-
-                }
-               
-            </div>
+                        </div>
+                    <button className="next-btn" onClick={handleNextBtnClick}>Next</button>    
+                </div>
+            </>
+            )}
         </section> 
     );
 }
