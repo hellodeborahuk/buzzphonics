1ccc19c4a24927a065f6e140a77abb24973a3688
Debbie Dann <29425781+hellodeborahuk@users.noreply.github.com>
2022-02-04 11:59:25 +0000

    Made randomEl a function

--- src/components/PhaseTwoQuiz.js
+++ src/components/PhaseTwoQuiz.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom"
 
 const PhaseTwoQuiz= () => {
 
+
     const quizQuestions = [
         {
             id: 0,
@@ -12,10 +13,9 @@ const PhaseTwoQuiz= () => {
             b: "d",
             c: "ck",
             answer: "d"
-        },
+        }
     ]
     const alternatingColor = ["var(--pink)", "var(--green)", "var(--yellow)", "var(--blue)", "var(--light-blue)", "var(--purple)"]
-    const randomElement = {backgroundColor: alternatingColor[Math.floor(Math.random() * alternatingColor.length)]}
 
     return ( 
         <section>
@@ -24,18 +24,34 @@ const PhaseTwoQuiz= () => {
                 <h1>Phase Two Quiz</h1>
             </header>
             <div className="quiz-container">
-                <div className="quiz-image-container" style={randomElement}><img className="quiz-image" src={`../images/${quizQuestions[0].image}.png`} alt={quizQuestions.alt} /></div>
-                <div className="quiz-options">
-                    <div className="sound" style={randomElement}>
-                        <p>{quizQuestions[0].a}</p>
-                    </div>
-                    <div className="sound" style={randomElement}>
-                        <p>{quizQuestions[0].b}</p>
-                    </div>   
-                    <div className="sound" style={randomElement}>
-                        <p>{quizQuestions[0].c}</p>
-                    </div>
-                </div>
+                {
+                    quizQuestions.map((quizQuestion, index) => {
+                        const randomElement = () => {
+                            return {backgroundColor: alternatingColor[Math.floor(Math.random() * alternatingColor.length)]}
+                        }
+                        return (
+                            <>
+                                <div className="quiz-image-container" style={randomElement()}>
+                                    <img className="quiz-image" src={`../images/${quizQuestion.image}.png`} alt={quizQuestion.alt} />
+                                    </div>
+                                    <div className="quiz-options">
+                                    <div className="sound" style={randomElement()}>
+                                        <p>{quizQuestion.a}</p>
+                                    </div>
+                                    <div className="sound" style={randomElement()}>
+                                        <p>{quizQuestion.b}</p>
+                                    </div>   
+                                    <div className="sound" style={randomElement()}>
+                                        <p>{quizQuestion.c}</p>
+                                    </div>
+                                </div>
+                                <button className="next-btn">Next</button>
+                             </>
+                       )
+                    })
+
+                }
+               
             </div>
         </section> 
     );
