be3da20f7958a91e71c5b8c8832743bafd0f1f69
Debbie Dann <29425781+hellodeborahuk@users.noreply.github.com>
2022-02-11 11:33:05 +0000

    Added shuffled deck to randomly assign 6 cards Closes #28
    Created shuffledDeck to choose 6 sets of sounds and icons.

--- src/components/MatchingGame.js
+++ src/components/MatchingGame.js
@@ -4,22 +4,33 @@ import { useNavigate } from "react-router-dom"
 
 const MatchingGame = () => {
   const navigate = useNavigate();
-    const sounds = [
+  
+  const sounds = [
         { id: 1, letter: "h", icon: "hat" },
         { id: 2, letter: "f", icon: "frog" },
         { id: 3, letter: "g", icon: "guitar" },
-        { id: 4, letter: "c", icon: "king" },
+        { id: 4, letter: "c", icon: "cat" },
         { id: 5, letter: "t", icon: "tap" },
-        { id: 6, letter: "s", icon: "sun" }
+        { id: 6, letter: "s", icon: "sun" },
+        { id: 7, letter: "p", icon: "penguin" },
+        { id: 8, letter: "u", icon: "umbrella" },
+        { id: 9, letter: "b", icon: "bin" },
+        { id: 10, letter: "e", icon: "egg" },
+        { id: 11, letter: "r", icon: "rat" },
+        { id: 12, letter: "a", icon: "ant" }
       ];
 
-   function shuffleCards() {
-    return [...sounds.map((sound) => {
+
+  function shuffleCards() {
+    const shuffledDeck = sounds.sort(() => Math.random() - 0.5).slice(0,6);
+
+    return [...shuffledDeck.map((sound) => {
       return {id: sound.id, letter: sound.letter}
-  }), ...sounds.map((sound) => {
+      }), ...shuffledDeck.map((sound) => {
       return {id: sound.id, icon: sound.icon}
-  })].sort(() => Math.random() - 0.5)
+      })].sort(() => Math.random() - 0.5)
    }
+
    const [openedCard, setOpenedCard] = useState([]);
    const [matched, setMatched] = useState([]);
    const [finished, setFinished] = useState(null)
